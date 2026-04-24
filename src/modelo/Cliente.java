e77d8fd8dff5ee2f374960318262d59908aca8e0
Ney Adrian <neyadrian2018@gmail.com>
2026-04-24 20:37:35 -0300

    sub classe empresa

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Cliente {
+
+    private String empresa;
+
+    public Cliente(String empresa) {
+        this.empresa = empresa;
+    }
+
+//    public String getTipo();
+}
