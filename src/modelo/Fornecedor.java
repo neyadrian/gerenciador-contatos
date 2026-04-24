be439bccc79820953251702a8e93455e4046c606
Ney Adrian <neyadrian2018@gmail.com>
2026-04-24 20:38:39 -0300

    sub classe servico

---
+++ src/modelo/Fornecedor.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Fornecedor {
+
+    private String servidor;
+
+    public Fornecedor(String servidor) {
+        this.servidor = servidor;
+    }
+
+    //    public String getTipo();
+}
