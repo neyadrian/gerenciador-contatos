32244cbb2cd8a144212e3b7680701503324dba7d
Ney Adrian <neyadrian2018@gmail.com>
2026-05-06 19:18:08 -0300

    update clientes

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,12 +1,32 @@
 package modelo;
 
-public class Cliente {
+public class Cliente extends Contato {
 
     private String empresa;
 
-    public Cliente(String empresa) {
+    public Cliente(String nome, String email, String empresa) {
+        super(nome, email);
         this.empresa = empresa;
     }
 
-//    public String getTipo();
+    public String getEmpresa() {
+        return empresa;
+    }
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    @Override
+    public String getTipo() {
+        return TipoContato.CLIENTE.name();
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + getNome() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", empresa='" + empresa + '\'' +
+                '}';
+    }
 }
