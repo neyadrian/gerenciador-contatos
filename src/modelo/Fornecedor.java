e45623dbc2293d837d22cc31884b5965e49fec2b
Ney Adrian <neyadrian2018@gmail.com>
2026-05-06 19:27:57 -0300

    update fornecedor

--- src/modelo/Fornecedor.java
+++ src/modelo/Fornecedor.java
@@ -1,12 +1,33 @@
 package modelo;
 
-public class Fornecedor {
+public class Fornecedor extends Contato{
 
-    private String servidor;
+    private String servico;
 
-    public Fornecedor(String servidor) {
-        this.servidor = servidor;
+    public Fornecedor(String nome, String email, String servidor) {
+        super(nome, email);
+        this.servico = servidor;
     }
 
-    //    public String getTipo();
+    public String getServico(){
+        return servico;
+    }
+
+    public void setServico(String servico) {
+        this.servico = servico;
+    }
+
+    @Override
+    public String getTipo() {
+        return TipoContato.FORNECEDOR.name();
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" +
+                "nome='" + getNome() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", servico='" + servico + '\'' +
+                '}';
+    }
 }
